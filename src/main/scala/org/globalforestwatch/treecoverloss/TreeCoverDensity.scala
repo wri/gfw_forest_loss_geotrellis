701ea7fd60fbc006e6659242952d969ee176b1ad
Thomas Maschler <tmaschler@wri.org>
2019-03-15 16:46:25 -0400

    TreeCoverDensity
    Signed-off-by: Thomas Maschler <tmaschler@wri.org>

---
+++ src/main/scala/org/globalforestwatch/treecoverloss/TreeCoverDensity.scala
@@ -0,0 +1,14 @@
+package org.globalforestwatch.treecoverloss
+
+object TreeCoverDensity {
+  def threshold(density: Int): Int = {
+    if (density < 10) 0
+    else if (density <15) 10
+    else if (density < 20) 15
+    else if (density <25) 20
+    else if (density <30) 25
+    else if (density <50) 30
+    else if (density <75) 50
+    else 75
+  }
+}
---
+++ src/test/scala/org/globalforestwatch/treecoverloss/TreeCoverDensitySuits.scala
@@ -0,0 +1,50 @@
+package org.globalforestwatch.treecoverloss
+
+
+import org.scalatest.FunSuite
+import TreeCoverDensity._
+
+class TreeCoverDensitySuits extends FunSuite {
+  test("Threshold 0") {
+    assert(threshold(0) === 0)
+    assert(threshold(9) === 0)
+  }
+
+  test("Threshold 10") {
+    assert(threshold(10) === 10)
+    assert(threshold(14) === 10)
+  }
+
+  test("Threshold 15") {
+    assert(threshold(15) === 15)
+    assert(threshold(19) === 15)
+  }
+
+
+  test("Threshold 20") {
+    assert(threshold(20) === 20)
+    assert(threshold(24) === 20)
+  }
+
+  test("Threshold 25") {
+    assert(threshold(25) === 25)
+    assert(threshold(29) === 25)
+  }
+
+  test("Threshold 30") {
+    assert(threshold(30) === 30)
+    assert(threshold(49) === 30)
+  }
+
+  test("Threshold 50") {
+    assert(threshold(50) === 50)
+    assert(threshold(74) === 50)
+  }
+
+
+  test("Threshold 75") {
+    assert(threshold(75) === 75)
+    assert(threshold(100) === 75)
+  }
+}
+
