5afd3aa35ec98df6d7a877bae95d2a7402938d2b
Thomas Maschler <tmaschler@wri.org>
2019-04-02 16:40:20 -0400

    rename fetchTile to fetchWindow
    Signed-off-by: Thomas Maschler <tmaschler@wri.org>

--- src/main/scala/org/globalforestwatch/treecoverloss/TenByTenGridSources.scala
+++ src/main/scala/org/globalforestwatch/treecoverloss/TenByTenGridSources.scala
@@ -26,7 +26,7 @@ class requiredTile(uri: String) {
     GeoTiffRasterSource(uri)
   }
 
-  def fetchTile(window: Extent):Tile = source.read(window).get.tile.band(0)
+  def fetchWindow(window: Extent):Tile = source.read(window).get.tile.band(0)
 
 }
 
@@ -47,7 +47,7 @@ class optionalTile(uri: String) {
     } else None
   }
 
-  def fetchTile(window: Extent):Option[Tile] = {
+  def fetchWindow(window: Extent):Option[Tile] = {
     for {
       source <- source
       raster <- Either.catchNonFatal(source.read(window).get.tile.band(0)).toOption
@@ -111,52 +111,52 @@ case class TenByTenGridSources(grid: String) extends LazyLogging {
     for {
       // Failure for any of these reads will result in function returning Left[Throwable]
       // These are effectively required fields without which we can't make sense of the analysis
-      loss <- Either.catchNonFatal(new requiredTile(mangroveBiomassSourceUri).fetchTile(window)).right
-      gain <- Either.catchNonFatal(new requiredTile(gainSourceUri).fetchTile(window)).right
-      tcd2000 <- Either.catchNonFatal(new requiredTile(tcd2000SourceUri).fetchTile(window)).right
-      tcd2010 <- Either.catchNonFatal(new requiredTile(tcd2010SourceUri).fetchTile(window)).right
-      co2Pixel <- Either.catchNonFatal(new requiredTile(co2PixelSourceUri).fetchTile(window)).right
-      biomass <- Either.catchNonFatal(new requiredTile(biomassSourceUri).fetchTile(window)).right
+      loss <- Either.catchNonFatal(new requiredTile(mangroveBiomassSourceUri).fetchWindow(window)).right
+      gain <- Either.catchNonFatal(new requiredTile(gainSourceUri).fetchWindow(window)).right
+      tcd2000 <- Either.catchNonFatal(new requiredTile(tcd2000SourceUri).fetchWindow(window)).right
+      tcd2010 <- Either.catchNonFatal(new requiredTile(tcd2010SourceUri).fetchWindow(window)).right
+      co2Pixel <- Either.catchNonFatal(new requiredTile(co2PixelSourceUri).fetchWindow(window)).right
+      biomass <- Either.catchNonFatal(new requiredTile(biomassSourceUri).fetchWindow(window)).right
 
     } yield {
       // Failure for these will be converted to optional result and propagated with TreeLossTile
-      val mangroveBiomass: Option[Tile] = new optionalTile(mangroveBiomassSourceUri).fetchTile(window)
-      val drivers: Option[Tile] = new optionalTile(driversSourceUri).fetchTile(window)
-      val globalLandCover: Option[Tile] = new optionalTile(globalLandCoverSourceUri).fetchTile(window)
-      val primaryForest: Option[Tile] = new optionalTile(primaryForestSourceUri).fetchTile(window)
-      val idnPrimaryForest: Option[Tile] = new optionalTile(idnPrimaryForestSourceUri).fetchTile(window)
-      val erosion: Option[Tile] = new optionalTile(erosionSourceUri).fetchTile(window)
-      val biodiversitySignificance: Option[Tile] = new optionalTile(biodiversitySignificanceSourceUri).fetchTile(window)
-      val wdpa: Option[Tile] = new optionalTile(wdpaSourceUri).fetchTile(window)
-      val plantations: Option[Tile] = new optionalTile(plantationsSourceUri).fetchTile(window)
-      val riverBasins: Option[Tile] = new optionalTile(riverBasinsSourceUri).fetchTile(window)
-      val ecozones: Option[Tile] = new optionalTile(ecozonesSourceUri).fetchTile(window)
-      val urbanWatersheds: Option[Tile] = new optionalTile(urbanWatershedsSourceUri).fetchTile(window)
-      val mangroves1996: Option[Tile] = new optionalTile(mangroves1996SourceUri).fetchTile(window)
-      val mangroves2016: Option[Tile] = new optionalTile(mangroves2016SourceUri).fetchTile(window)
-      val waterStress: Option[Tile] = new optionalTile(waterStressSourceUri).fetchTile(window)
-      val intactForestLandscapes: Option[Tile] = new optionalTile(intactForestLandscapesSourceUri).fetchTile(window)
-      val endemicBirdAreas: Option[Tile] = new optionalTile(endemicBirdAreasSourceUri).fetchTile(window)
-      val tigerLandscapes: Option[Tile] = new optionalTile(tigerLandscapesSourceUri).fetchTile(window)
-      val landmark: Option[Tile] = new optionalTile(landmarkSourceUri).fetchTile(window)
-      val landRights: Option[Tile] = new optionalTile(landRightsSourceUri).fetchTile(window)
-      val keyBiodiversityAreas: Option[Tile] = new optionalTile(keyBiodiversityAreasSourceUri).fetchTile(window)
-      val mining: Option[Tile] = new optionalTile(miningSourceUri).fetchTile(window)
-      val rspo: Option[Tile] = new optionalTile(rspoSourceUri).fetchTile(window)
-      val peatlands: Option[Tile] = new optionalTile(peatlandsSourceUri).fetchTile(window)
-      val oilPalm: Option[Tile] = new optionalTile(oilPalmSourceUri).fetchTile(window)
-      val idnForestMoratorium: Option[Tile] = new optionalTile(idnForestMoratoriumSourceUri).fetchTile(window)
-      val idnLandCover: Option[Tile] = new optionalTile(idnLandCoverSourceUri).fetchTile(window)
-      val mexProtectedAreas: Option[Tile] = new optionalTile(mexProtectedAreasSourceUri).fetchTile(window)
-      val mexPaymentForEcosystemServices: Option[Tile] = new optionalTile(mexPaymentForEcosystemServicesSourceUri).fetchTile(window)
-      val mexForestZoning: Option[Tile] = new optionalTile(mexForestZoningSourceUri).fetchTile(window)
-      val perProductionForest: Option[Tile] = new optionalTile(perProductionForestSourceUri).fetchTile(window)
-      val perProtectedAreas: Option[Tile] = new optionalTile(perProtectedAreasSourceUri).fetchTile(window)
-      val perForestConcessions: Option[Tile] = new optionalTile(perForestConcessionsSourceUri).fetchTile(window)
-      val braBiomes: Option[Tile] = new optionalTile(braBiomesSourceUri).fetchTile(window)
-      val woodFiber: Option[Tile] = new optionalTile(woodFiberSourceUri).fetchTile(window)
-      val resourceRights: Option[Tile] = new optionalTile(resourceRightsSourceUri).fetchTile(window)
-      val logging: Option[Tile] = new optionalTile(loggingSourceUri).fetchTile(window)
+      val mangroveBiomass: Option[Tile] = new optionalTile(mangroveBiomassSourceUri).fetchWindow(window)
+      val drivers: Option[Tile] = new optionalTile(driversSourceUri).fetchWindow(window)
+      val globalLandCover: Option[Tile] = new optionalTile(globalLandCoverSourceUri).fetchWindow(window)
+      val primaryForest: Option[Tile] = new optionalTile(primaryForestSourceUri).fetchWindow(window)
+      val idnPrimaryForest: Option[Tile] = new optionalTile(idnPrimaryForestSourceUri).fetchWindow(window)
+      val erosion: Option[Tile] = new optionalTile(erosionSourceUri).fetchWindow(window)
+      val biodiversitySignificance: Option[Tile] = new optionalTile(biodiversitySignificanceSourceUri).fetchWindow(window)
+      val wdpa: Option[Tile] = new optionalTile(wdpaSourceUri).fetchWindow(window)
+      val plantations: Option[Tile] = new optionalTile(plantationsSourceUri).fetchWindow(window)
+      val riverBasins: Option[Tile] = new optionalTile(riverBasinsSourceUri).fetchWindow(window)
+      val ecozones: Option[Tile] = new optionalTile(ecozonesSourceUri).fetchWindow(window)
+      val urbanWatersheds: Option[Tile] = new optionalTile(urbanWatershedsSourceUri).fetchWindow(window)
+      val mangroves1996: Option[Tile] = new optionalTile(mangroves1996SourceUri).fetchWindow(window)
+      val mangroves2016: Option[Tile] = new optionalTile(mangroves2016SourceUri).fetchWindow(window)
+      val waterStress: Option[Tile] = new optionalTile(waterStressSourceUri).fetchWindow(window)
+      val intactForestLandscapes: Option[Tile] = new optionalTile(intactForestLandscapesSourceUri).fetchWindow(window)
+      val endemicBirdAreas: Option[Tile] = new optionalTile(endemicBirdAreasSourceUri).fetchWindow(window)
+      val tigerLandscapes: Option[Tile] = new optionalTile(tigerLandscapesSourceUri).fetchWindow(window)
+      val landmark: Option[Tile] = new optionalTile(landmarkSourceUri).fetchWindow(window)
+      val landRights: Option[Tile] = new optionalTile(landRightsSourceUri).fetchWindow(window)
+      val keyBiodiversityAreas: Option[Tile] = new optionalTile(keyBiodiversityAreasSourceUri).fetchWindow(window)
+      val mining: Option[Tile] = new optionalTile(miningSourceUri).fetchWindow(window)
+      val rspo: Option[Tile] = new optionalTile(rspoSourceUri).fetchWindow(window)
+      val peatlands: Option[Tile] = new optionalTile(peatlandsSourceUri).fetchWindow(window)
+      val oilPalm: Option[Tile] = new optionalTile(oilPalmSourceUri).fetchWindow(window)
+      val idnForestMoratorium: Option[Tile] = new optionalTile(idnForestMoratoriumSourceUri).fetchWindow(window)
+      val idnLandCover: Option[Tile] = new optionalTile(idnLandCoverSourceUri).fetchWindow(window)
+      val mexProtectedAreas: Option[Tile] = new optionalTile(mexProtectedAreasSourceUri).fetchWindow(window)
+      val mexPaymentForEcosystemServices: Option[Tile] = new optionalTile(mexPaymentForEcosystemServicesSourceUri).fetchWindow(window)
+      val mexForestZoning: Option[Tile] = new optionalTile(mexForestZoningSourceUri).fetchWindow(window)
+      val perProductionForest: Option[Tile] = new optionalTile(perProductionForestSourceUri).fetchWindow(window)
+      val perProtectedAreas: Option[Tile] = new optionalTile(perProtectedAreasSourceUri).fetchWindow(window)
+      val perForestConcessions: Option[Tile] = new optionalTile(perForestConcessionsSourceUri).fetchWindow(window)
+      val braBiomes: Option[Tile] = new optionalTile(braBiomesSourceUri).fetchWindow(window)
+      val woodFiber: Option[Tile] = new optionalTile(woodFiberSourceUri).fetchWindow(window)
+      val resourceRights: Option[Tile] = new optionalTile(resourceRightsSourceUri).fetchWindow(window)
+      val logging: Option[Tile] = new optionalTile(loggingSourceUri).fetchWindow(window)
 
       val tile = TreeLossTile(
         loss,
