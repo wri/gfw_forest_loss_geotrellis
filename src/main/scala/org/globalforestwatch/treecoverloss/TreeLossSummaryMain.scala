7d6364d14ee088c47e528ce9ff0cf95b2689f87d
Thomas Maschler <tmaschler@wri.org>
2019-04-18 02:07:21 -0400

    export iso, adm1, adm2
    Signed-off-by: Thomas Maschler <tmaschler@wri.org>

--- src/main/scala/org/globalforestwatch/treecoverloss/TreeLossSummaryMain.scala
+++ src/main/scala/org/globalforestwatch/treecoverloss/TreeLossSummaryMain.scala
@@ -126,8 +126,8 @@ object TreeLossSummaryMain extends CommandApp (
         }.toDF("feature_id",
           "threshold_2000", "threshold_2010", "layers",
           "area", "gain",
-          "total_biomass", "total_co2", "mean_biomass_per_ha",
-          "total_mangrove_biomass", "total_mangrove_co2", "mean_mangrove_biomass_per_ha", "year_data")
+          "biomass", "co2", "biomass_per_ha",
+          "mangrove_biomass", "mangrove_co2", "mangrove_biomass_per_ha", "year_data")
 
       var sparkConf: SparkConf = null
       sparkConf = new SparkConf().set("spark.sql.crossJoin.enabled", "true")
@@ -143,12 +143,12 @@ object TreeLossSummaryMain extends CommandApp (
       val s2000DF = summaryDF
         .select(summaryDF.columns.map(c => col(c).as(lookup2000.getOrElse(c, c))): _*)
         .groupBy("feature_id", "layers", "threshold")
-        .agg(sum("area") as "area")
+        .agg(sum("area") as "area", sum("biomass") as "biomass", sum("co2") as "co2", sum("mangrove_biomass") as "mangrove_biomass", sum("mangrove_co2") as "mangrove_co2")
       val t2000DF = s2000DF
         .join(tAdm2DF, tAdm2DF("feature_id") <=> s2000DF("feature_id")
           && tAdm2DF("layers") <=> s2000DF("layers") && tAdm2DF("threshold") <=> s2000DF("threshold")
           , "right_outer")
-        .select(tAdm2DF("feature_id"), tAdm2DF("threshold"), tAdm2DF("layers"), s2000DF("area"))
+        .select(tAdm2DF("feature_id"), tAdm2DF("threshold"), tAdm2DF("layers"), s2000DF("area"), $"biomass", $"co2", $"mangrove_biomass", $"mangrove_co2")
 
       val lookup2010 = Map("threshold_2010" -> "threshold")
       val s2010DF = summaryDF
@@ -165,24 +165,40 @@ object TreeLossSummaryMain extends CommandApp (
       val WindowPartitionOrder = Window.partitionBy($"feature_id", $"layers").orderBy($"threshold".desc)
 
       val tcExtent = sum("area").over(WindowPartitionOrder)
-      val biomassSum = sum("total_biomass").over(WindowPartitionOrder)
-      val co2Sum = sum("totalCo2").over(WindowPartitionOrder)
+      val biomassSum = sum("biomass").over(WindowPartitionOrder)
+      val co2Sum = sum("co2").over(WindowPartitionOrder)
+      val mangroveBiomassSum = sum("mangrove_biomass").over(WindowPartitionOrder)
+      val mangroveCo2Sum = sum("mangrove_co2").over(WindowPartitionOrder)
 
-      val u2000DF = t2000DF.select($"*", tcExtent as "extent2000")
+      val u2000DF = t2000DF.select($"*", tcExtent as "extent2000", biomassSum as "total_biomass", co2Sum as "total_co2", mangroveBiomassSum as "total_mangrove_biomass", mangroveCo2Sum as "total_mangrove_co2")
       val u2010DF = t2010DF.select($"*", tcExtent as "extent2010")
 
-      val finalDF = tAdm2DF.
+      val adm2DF = tAdm2DF.
         join(u2000DF, tAdm2DF("feature_id") <=> u2000DF("feature_id")
           && tAdm2DF("layers") <=> u2000DF("layers") && tAdm2DF("threshold") <=> u2000DF("threshold")
           , "left_outer")
-        .select(tAdm2DF("feature_id"), tAdm2DF("layers"), tAdm2DF("threshold"), $"totalarea", $"extent2000")
+        .select(tAdm2DF("feature_id"), tAdm2DF("layers"), tAdm2DF("threshold"), $"totalarea", $"extent2000", u2000DF("total_biomass"), u2000DF("total_co2"), u2000DF("total_mangrove_biomass"), u2000DF("total_mangrove_co2"))
         .join(u2010DF, tAdm2DF("feature_id") <=> u2010DF("feature_id") && tAdm2DF("threshold") <=> u2010DF("threshold")
           && u2010DF("layers") <=> tAdm2DF("layers")
           , "left_outer")
-        .select(tAdm2DF("feature_id"), tAdm2DF("threshold"), tAdm2DF("layers"), $"totalarea", $"extent2000", $"extent2010")
+        .select(tAdm2DF("feature_id"), tAdm2DF("threshold"), tAdm2DF("layers"), $"totalarea", $"extent2000", $"extent2010", $"total_biomass", $"total_co2", $"total_mangrove_biomass", $"total_mangrove_co2")
         .groupBy("feature_id", "threshold", "layers")
-        .agg(sum("totalarea") as "totalarea", sum("extent2000") as "extent2000", sum("extent2010") as "extent2010")
-        .select($"feature_id.iso".alias("iso"),
+        .agg(sum("totalarea") as "totalarea", sum("extent2000") as "extent2000", sum("extent2010") as "extent2010",
+          sum("total_biomass") as "total_biomass", sum("total_co2") as "total_co2", sum("total_mangrove_biomass") as "total_mangrove_biomass", sum("total_mangrove_co2") as "total_mangrove_co2")
+
+      val outputPartitionCount = maybeOutputPartitions.getOrElse(featureRDD.getNumPartitions)
+
+      //val jsonDs = summaryDF.repartition(outputPartitionCount).toJSON
+      //      finalDF.
+      //        repartition(1).
+      //        toJSON. //.repartition(outputPartitionCount).toJSON
+      //        //mapPartitions(vals => Iterator("[" + vals.mkString(",") + "]")).
+      //        write.
+      //        text(outputUrl)
+
+
+      adm2DF.
+        select($"feature_id.iso".alias("iso"),
           $"feature_id.adm1".alias("adm1"),
           $"feature_id.adm2".alias("adm2"),
           $"threshold",
@@ -227,23 +243,126 @@ object TreeLossSummaryMain extends CommandApp (
           $"layers.oilGas".alias("oil_gas"),
           $"totalarea",
           $"extent2000",
-          $"extent2010"
-        )
-      val outputPartitionCount = maybeOutputPartitions.getOrElse(featureRDD.getNumPartitions)
-
-      //val jsonDs = summaryDF.repartition(outputPartitionCount).toJSON
-      //      finalDF.
-      //        repartition(1).
-      //        toJSON. //.repartition(outputPartitionCount).toJSON
-      //        //mapPartitions(vals => Iterator("[" + vals.mkString(",") + "]")).
-      //        write.
-      //        text(outputUrl)
-
-
-      finalDF.
+          $"extent2010",
+          $"total_biomass", $"total_co2", $"total_mangrove_biomass", $"total_mangrove_co2"
+        ).
         repartition(1).
         write.options(Map("header" -> "true", "delimiter" -> "\t", "quote" -> "\u0000", "quoteMode" -> "NONE", "nullValue" -> "\u0000")). // unicode for nothing. tried "quoteMode" -> "NONE" but didn't work
-        csv(path = outputUrl)
+        csv(path = outputUrl + "/adm2")
+
+
+      val adm1DF = adm2DF.groupBy("feature_id.iso", "feature_id.adm1", "layers", "threshold")
+        .agg(sum("totalarea") as "totalarea", sum("extent2000") as "extent2000", sum("extent2010") as "extent2010",
+          sum("total_biomass") as "total_biomass", sum("total_co2") as "total_co2", sum("total_mangrove_biomass") as "total_mangrove_biomass", sum("total_mangrove_co2") as "total_mangrove_co2")
+
+
+      adm1DF.
+        select($"iso",
+          $"adm1",
+          $"threshold",
+          $"layers.drivers".alias("tcs"),
+          $"layers.globalLandCover".alias("global_land_cover"),
+          $"layers.primaryForest".alias("primary_forest"),
+          $"layers.idnPrimaryForest".alias("idn_primary_forest"),
+          $"layers.erosion".alias("erosion"),
+          $"layers.biodiversitySignificance".alias("biodiversity_significance"),
+          $"layers.biodiversityIntactness".alias("biodiversity_intactness"),
+          $"layers.wdpa".alias("wdpa"),
+          $"layers.aze".alias("aze"),
+          $"layers.plantations".alias("plantations"),
+          $"layers.riverBasins".alias("river_basin"),
+          $"layers.ecozones".alias("ecozone"),
+          $"layers.urbanWatersheds".alias("urban_watershed"),
+          $"layers.mangroves1996".alias("mangroves_1996"),
+          $"layers.mangroves2016".alias("mangroves_2016"),
+          $"layers.waterStress".alias("water_stress"),
+          $"layers.intactForestLandscapes".alias("ifl"),
+          $"layers.endemicBirdAreas".alias("endemic_bird_area"),
+          $"layers.tigerLandscapes".alias("tiger_cl"),
+          $"layers.landmark".alias("landmark"),
+          $"layers.landRights".alias("land_right"),
+          $"layers.keyBiodiversityAreas".alias("kba"),
+          $"layers.mining".alias("mining"),
+          $"layers.rspo".alias("rspo"),
+          $"layers.peatlands".alias("idn_mys_peatlands"),
+          $"layers.oilPalm".alias("oil_palm"),
+          $"layers.idnForestMoratorium".alias("idn_forest_moratorium"),
+          $"layers.idnLandCover".alias("idn_land_cover"),
+          $"layers.mexProtectedAreas".alias("mex_protected_areas"),
+          $"layers.mexPaymentForEcosystemServices".alias("mex_pes"),
+          $"layers.mexForestZoning".alias("mex_forest_zoning"),
+          $"layers.perProductionForest".alias("per_production_forest"),
+          $"layers.perProtectedAreas".alias("per_protected_area"),
+          $"layers.perForestConcessions".alias("per_forest_concession"),
+          $"layers.braBiomes".alias("bra_biomes"),
+          $"layers.woodFiber".alias("wood_fiber"),
+          $"layers.resourceRights".alias("resource_right"),
+          $"layers.logging".alias("managed_forests"),
+          $"layers.oilGas".alias("oil_gas"),
+          $"totalarea",
+          $"extent2000",
+          $"extent2010",
+          $"total_biomass", $"total_co2", $"total_mangrove_biomass", $"total_mangrove_co2"
+        ).
+        repartition(1).
+        write.options(Map("header" -> "true", "delimiter" -> "\t", "quote" -> "\u0000", "quoteMode" -> "NONE", "nullValue" -> "\u0000")). // unicode for nothing. tried "quoteMode" -> "NONE" but didn't work
+        csv(path = outputUrl + "/adm1")
+
+      val isoDF = adm1DF.groupBy("iso", "layers", "threshold")
+        .agg(sum("totalarea") as "totalarea", sum("extent2000") as "extent2000", sum("extent2010") as "extent2010",
+          sum("total_biomass") as "total_biomass", sum("total_co2") as "total_co2", sum("total_mangrove_biomass") as "total_mangrove_biomass", sum("total_mangrove_co2") as "total_mangrove_co2")
+
+
+      isoDF.
+        select($"iso",
+          $"threshold",
+          $"layers.drivers".alias("tcs"),
+          $"layers.globalLandCover".alias("global_land_cover"),
+          $"layers.primaryForest".alias("primary_forest"),
+          $"layers.idnPrimaryForest".alias("idn_primary_forest"),
+          $"layers.erosion".alias("erosion"),
+          $"layers.biodiversitySignificance".alias("biodiversity_significance"),
+          $"layers.biodiversityIntactness".alias("biodiversity_intactness"),
+          $"layers.wdpa".alias("wdpa"),
+          $"layers.aze".alias("aze"),
+          $"layers.plantations".alias("plantations"),
+          $"layers.riverBasins".alias("river_basin"),
+          $"layers.ecozones".alias("ecozone"),
+          $"layers.urbanWatersheds".alias("urban_watershed"),
+          $"layers.mangroves1996".alias("mangroves_1996"),
+          $"layers.mangroves2016".alias("mangroves_2016"),
+          $"layers.waterStress".alias("water_stress"),
+          $"layers.intactForestLandscapes".alias("ifl"),
+          $"layers.endemicBirdAreas".alias("endemic_bird_area"),
+          $"layers.tigerLandscapes".alias("tiger_cl"),
+          $"layers.landmark".alias("landmark"),
+          $"layers.landRights".alias("land_right"),
+          $"layers.keyBiodiversityAreas".alias("kba"),
+          $"layers.mining".alias("mining"),
+          $"layers.rspo".alias("rspo"),
+          $"layers.peatlands".alias("idn_mys_peatlands"),
+          $"layers.oilPalm".alias("oil_palm"),
+          $"layers.idnForestMoratorium".alias("idn_forest_moratorium"),
+          $"layers.idnLandCover".alias("idn_land_cover"),
+          $"layers.mexProtectedAreas".alias("mex_protected_areas"),
+          $"layers.mexPaymentForEcosystemServices".alias("mex_pes"),
+          $"layers.mexForestZoning".alias("mex_forest_zoning"),
+          $"layers.perProductionForest".alias("per_production_forest"),
+          $"layers.perProtectedAreas".alias("per_protected_area"),
+          $"layers.perForestConcessions".alias("per_forest_concession"),
+          $"layers.braBiomes".alias("bra_biomes"),
+          $"layers.woodFiber".alias("wood_fiber"),
+          $"layers.resourceRights".alias("resource_right"),
+          $"layers.logging".alias("managed_forests"),
+          $"layers.oilGas".alias("oil_gas"),
+          $"totalarea",
+          $"extent2000",
+          $"extent2010",
+          $"total_biomass", $"total_co2", $"total_mangrove_biomass", $"total_mangrove_co2"
+        ).
+        repartition(1).
+        write.options(Map("header" -> "true", "delimiter" -> "\t", "quote" -> "\u0000", "quoteMode" -> "NONE", "nullValue" -> "\u0000")). // unicode for nothing. tried "quoteMode" -> "NONE" but didn't work
+        csv(path = outputUrl + "/iso")
 
       spark.stop
     }
