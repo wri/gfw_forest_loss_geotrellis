91954daa221ec767e117023a57029b6b8014e79d
Thomas Maschler <tmaschler@wri.org>
2019-12-20 18:01:35 -0500

    add whitelist to annualupdate_minimal and carbonflux analysis
    Signed-off-by: Thomas Maschler <tmaschler@wri.org>

--- src/main/scala/org/globalforestwatch/summarystats/annualupdate_minimal/dataframes/Adm1ApiDF.scala
+++ src/main/scala/org/globalforestwatch/summarystats/annualupdate_minimal/dataframes/Adm1ApiDF.scala
@@ -1,6 +1,6 @@
 package org.globalforestwatch.summarystats.annualupdate_minimal.dataframes
 
-import org.apache.spark.sql.functions.{length, max, sum}
+import org.apache.spark.sql.functions.{max, sum}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object Adm1ApiDF {
@@ -136,27 +136,23 @@ object Adm1ApiDF {
 
     df.groupBy($"iso", $"adm1")
       .agg(
-        max(length($"tcs_driver__type")).cast("boolean") as "tcs_driver__type",
-        max(length($"global_land_cover__class"))
-          .cast("boolean") as "global_land_cover__class",
+        max($"tcs_driver__type") as "tcs_driver__type",
+        max($"global_land_cover__class") as "global_land_cover__class",
         max($"is__regional_primary_forest") as "is__regional_primary_forest",
         //      $"is__idn_primary_forest",
         //      $"erosion_risk__level",
         //      $"is__biodiversity_significance_top_10_perc",
         //      $"is__biodiversity_intactness_top_10_perc",
-        max(length($"wdpa_protected_area__iucn_cat"))
-          .cast("boolean") as "wdpa_protected_area__iucn_cat",
+        max($"wdpa_protected_area__iucn_cat") as "wdpa_protected_area__iucn_cat",
         max($"is__alliance_for_zero_extinction_site") as "is__alliance_for_zero_extinction_site",
-        max(length($"gfw_plantation__type"))
-          .cast("boolean") as "gfw_plantation__type",
+        max($"gfw_plantation__type") as "gfw_plantation__type",
         //      $"river_basin__name",
         //      $"ecozone__name",
         //      $"is__urban_water_intake",
         max($"is__mangroves_1996") as "is__mangroves_1996",
         max($"is__mangroves_2016") as "is__mangroves_2016",
         //      $"baseline_water_stress__level",
-        max(length($"intact_forest_landscape__year"))
-          .cast("boolean") as "intact_forest_landscape__year",
+        max($"intact_forest_landscape__year") as "intact_forest_landscape__year",
         //      $"is__endemic_bird_area",
         max($"is__tiger_conservation_landscape") as "is__tiger_conservation_landscape",
         max($"is__landmark") as "is__landmark",
--- src/main/scala/org/globalforestwatch/summarystats/carbonflux/dataframes/IsoApiDF.scala
+++ src/main/scala/org/globalforestwatch/summarystats/carbonflux/dataframes/IsoApiDF.scala
@@ -1,6 +1,6 @@
 package org.globalforestwatch.summarystats.carbonflux.dataframes
 
-import org.apache.spark.sql.functions.{length, max, sum}
+import org.apache.spark.sql.functions.{max, sum}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object IsoApiDF {
@@ -88,19 +88,15 @@ object IsoApiDF {
 
     df.groupBy($"iso")
       .agg(
-        max(length($"tcs_driver__type")).cast("boolean") as "tcs_driver__type",
         max($"is__treecover_loss_2000-2015") as "is__treecover_loss_2000-2015",
         max($"is__treecover_gain_2000-2012") as "is__treecover_gain_2000-2012",
         max($"is__mangrove") as "is__mangrove",
-        max(length($"tcs_driver__type")).cast("boolean") as "tcs_driver__type",
-        max(length($"ecozone__name")).cast("boolean") as "ecozone__name",
+        max($"tcs_driver__type") as "tcs_driver__type",
+        max($"ecozone__name") as "ecozone__name",
         max($"is__gfw_land_right") as "is__gfw_land_right",
-        max(length($"wdpa_protected_area__iucn_cat"))
-          .cast("boolean") as "wdpa_protected_area__iucn_cat",
-        max(length($"intact_forest_landscape__year"))
-          .cast("boolean") as "intact_forest_landscape__year",
-        max(length($"gfw_plantation__type"))
-          .cast("boolean") as "gfw_plantation__type",
+        max($"wdpa_protected_area__iucn_cat") as "wdpa_protected_area__iucn_cat",
+        max($"intact_forest_landscape__year") as "intact_forest_landscape__year",
+        max($"gfw_plantation__type") as "gfw_plantation__type",
         max($"is__intact_primary_forest") as "is__intact_primary_forest",
         max($"is__peatlands_flux") as "is__peatlands_flux"
       )
