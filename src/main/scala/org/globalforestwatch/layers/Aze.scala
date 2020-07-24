8ea6055553518f3f9e557c43e319ec4d6b278289
Thomas Maschler <tmaschler@wri.org>
2020-07-23 20:49:10 -0400

    fix version number
    Signed-off-by: Thomas Maschler <tmaschler@wri.org>

--- src/main/scala/org/globalforestwatch/layers/Aze.scala
+++ src/main/scala/org/globalforestwatch/layers/Aze.scala
@@ -3,5 +3,5 @@ package org.globalforestwatch.layers
 import org.globalforestwatch.grids.{GridTile}
 
 case class Aze(gridTile: GridTile) extends BooleanLayer with OptionalILayer {
-  val uri: String = s"$basePath/birdlife_alliance_for_zero_extinction_site/v20190816/raster/epsg-4326/${gridTile.gridSize}/${gridTile.rowCount}/is/geotiff/${gridTile.tileId}.tif"
+  val uri: String = s"$basePath/birdlife_alliance_for_zero_extinction_sites/v20190816/raster/epsg-4326/${gridTile.gridSize}/${gridTile.rowCount}/is/geotiff/${gridTile.tileId}.tif"
 }
