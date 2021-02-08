0d522517470229af6ea7186a6ae12e10ea8ec068
Thomas Maschler <tmaschler@wri.org>
2021-02-08 13:31:18 -0500

    resort se asia class mapping
    Signed-off-by: Thomas Maschler <tmaschler@wri.org>

--- src/main/scala/org/globalforestwatch/layers/SEAsiaLandCover.scala
+++ src/main/scala/org/globalforestwatch/layers/SEAsiaLandCover.scala
@@ -13,22 +13,21 @@ case class SEAsiaLandCover(gridTile: GridTile)
   override val externalNoDataValue = "Unknown"
 
   def lookup(value: Int): String = value match {
-    case 19 => "Bare land"
-    case 18 => "Coastal fish pond"
+    case 1 | 3 | 4 => "Primary forest"
+    case 2 | 5 | 6 => "Secondary forest"
     case 7 => "Rubber plantation"
-    case 13 | 17 => "Agriculture"
-    case 2 | 6 | 5 => "Secondary forest"
-    case 15 | 11 => "Grassland/ shrub"
-    case 20 => "Mining"
-    case 10 => "Mixed tree crops"
-    case 22 => "No data"
     case 8 => "Oil palm plantation"
-    case 14 => "Settlements"
-    case 16 | 12 => "Swamp"
     case 9 => "Timber plantation"
-    case 1 | 4 | 3 => "Primary forest"
+    case 10 => "Mixed tree crops"
+    case 11 | 15 => "Grassland/ shrub"
+    case 12 | 16 => "Swamp"
+    case 13 | 17 => "Agriculture"
+    case 14 => "Settlements"
+    case 18 => "Coastal fish pond"
+    case 19 => "Bare land"
+    case 20 => "Mining"
     case 21 => "Water bodies"
-
-
+    case 22 => "No data"
+    case _ => "Unknown"
   }
 }
