b9af61c2136cf4c17e179969fcf6001e91786606
Thomas Maschler <tmaschler@wri.org>
2021-02-09 15:55:36 -0500

    fix sea landcover path
    Signed-off-by: Thomas Maschler <tmaschler@wri.org>

--- src/main/scala/org/globalforestwatch/layers/SEAsiaLandCover.scala
+++ src/main/scala/org/globalforestwatch/layers/SEAsiaLandCover.scala
@@ -6,9 +6,8 @@ case class SEAsiaLandCover(gridTile: GridTile)
     extends StringLayer
     with OptionalILayer {
 
-  //FIXME: Need to verify data lake path
   val uri: String =
-    s"$basePath/south_east_asia_land_cover/v2015/raster/epsg-4326/${gridTile.gridSize}/${gridTile.rowCount}/name/gdal-geotiff/${gridTile.tileId}.tif"
+    s"$basePath/rspo_southeast_asia_land_cover_2010/v2013/raster/epsg-4326/${gridTile.gridSize}/${gridTile.rowCount}/land_cover_class/gdal-geotiff/${gridTile.tileId}.tif"
 
   override val externalNoDataValue = "Unknown"
 
