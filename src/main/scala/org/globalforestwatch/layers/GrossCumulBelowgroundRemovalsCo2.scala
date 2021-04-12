05be8d11c6e4a204ebdb97f6c35af3bc153c1afc
justin.terry@wri.org <jt@c02xrkdcjg5h.myfiosgateway.com>
2021-04-12 14:27:48 -0400

    Use 2020 drivers and carbon model layers

--- src/main/scala/org/globalforestwatch/layers/GrossCumulBelowgroundRemovalsCo2.scala
+++ src/main/scala/org/globalforestwatch/layers/GrossCumulBelowgroundRemovalsCo2.scala
@@ -8,6 +8,6 @@ case class GrossCumulBelowgroundRemovalsCo2(gridTile: GridTile, model: String="s
   val model_suffix: String = if (model == "standard") "standard" else s"$model"
   val uri: String =
 //    s"$basePath/gfw_gross_cumul_belowground_removals_co2$model_suffix/v20191106/raster/epsg-4326/${gridTile.gridSize}/${gridTile.rowCount}//Mg_ha-1/geotiff/${gridTile.tileId}.tif"
-//    s"s3://gfw-files/flux_1_2_1/gross_removals_BGCO2_all_forest_types/$model_suffix/${gridTile.tileId}.tif"
-    s"s3://gfw-files/flux_1_2_0/gross_removals_BGCO2_all_forest_types/$model_suffix/${gridTile.tileId}.tif"
+    s"s3://gfw-files/flux_1_2_1/gross_removals_BGCO2_all_forest_types/$model_suffix/${gridTile.tileId}.tif"
+//    s"s3://gfw-files/flux_1_2_0/gross_removals_BGCO2_all_forest_types/$model_suffix/${gridTile.tileId}.tif"
 }
