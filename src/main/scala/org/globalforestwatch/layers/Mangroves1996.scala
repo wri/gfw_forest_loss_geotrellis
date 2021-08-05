ceb904d884e92e2789dca6a1364b896a5dbf446c
Justin Terry <justin.terry@wri.org>
2021-08-05 11:05:59 -0700

    Use correct dataset name for mangroves

--- src/main/scala/org/globalforestwatch/layers/Mangroves1996.scala
+++ src/main/scala/org/globalforestwatch/layers/Mangroves1996.scala
@@ -5,7 +5,7 @@ import org.globalforestwatch.grids.GridTile
 case class Mangroves1996(gridTile: GridTile, kwargs: Map[String, Any])
   extends BooleanLayer
     with OptionalILayer {
-  val datasetName = "gmw_mangroves_1996"
+  val datasetName = "gmw_global_mangrove_extent_1996"
   val uri: String =
     s"$basePath/$datasetName/$version/raster/epsg-4326/${gridTile.gridSize}/${gridTile.rowCount}/is/geotiff/${gridTile.tileId}.tif"
 }
--- src/main/scala/org/globalforestwatch/layers/Mangroves2016.scala
+++ src/main/scala/org/globalforestwatch/layers/Mangroves2016.scala
@@ -6,7 +6,7 @@ case class Mangroves2016(gridTile: GridTile, kwargs: Map[String, Any])
   extends BooleanLayer
     with OptionalILayer {
 
-  val datasetName = "gmw_mangroves_2016"
+  val datasetName = "gmw_global_mangrove_extent_2016"
   val uri: String =
     s"$basePath/$datasetName/$version/raster/epsg-4326/${gridTile.gridSize}/${gridTile.rowCount}/is/geotiff/${gridTile.tileId}.tif"
 }
