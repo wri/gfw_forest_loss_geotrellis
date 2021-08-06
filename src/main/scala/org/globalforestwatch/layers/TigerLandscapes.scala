1c806dc5dc7609a992f29254d84391f1987caf3c
manukala6 <manukala6@gmail.com>
2021-08-06 09:38:31 -0400

    Revert to prev version

--- src/main/scala/org/globalforestwatch/layers/TigerLandscapes.scala
+++ src/main/scala/org/globalforestwatch/layers/TigerLandscapes.scala
@@ -2,10 +2,6 @@ package org.globalforestwatch.layers
 
 import org.globalforestwatch.grids.GridTile
 
-case class TigerLandscapes(gridTile: GridTile, kwargs: Map[String, Any])
-  extends BooleanLayer
-    with OptionalILayer {
-  val datasetName = "wwf_tiger_conservation_landscapes"
-  val uri: String =
-    s"$basePath/gfw_tiger_landscapes/v20201207/raster/epsg-4326/${gridTile.gridSize}/${gridTile.rowCount}/is/geotiff/${gridTile.tileId}.tif"
+case class TigerLandscapes(gridTile: GridTile) extends BooleanLayer with OptionalILayer {
+  val uri: String = s"$basePath/gfw_tiger_landscapes/v201904/raster/epsg-4326/${gridTile.gridSize}/${gridTile.rowCount}/is/geotiff/${gridTile.tileId}.tif"
 }
