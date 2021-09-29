a4437fa738a2825a16140c7212039648e77a716e
Eugene Cheipesh <echeipesh@gmail.com>
2021-09-29 09:41:56 -0400

    Spark 3.1.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Version {
   val geotrellis = "3.6.0"
   val hadoop = "3.2.1"
   val jackson = "2.10.0"
-  val spark = "3.0.0"
+  val spark = "3.1.1"
   val sparkCompatible = "3.0"
   val geotools = "23.1"
   val sedona = "1.0.0-incubating"
