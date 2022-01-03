52698aa028abef63db3338729dad532c2447aeba
Eugene Cheipesh <echeipesh@gmail.com>
2022-01-03 14:18:14 -0500

    Bad argumnets produce exit code 2

--- src/main/scala/org/globalforestwatch/summarystats/SummaryMain.scala
+++ src/main/scala/org/globalforestwatch/summarystats/SummaryMain.scala
@@ -1,6 +1,5 @@
 package org.globalforestwatch.summarystats
 
-import com.monovore.decline.CommandApp
 import org.globalforestwatch.summarystats.annualupdate_minimal.AnnualUpdateMinimalCommand.annualupdateMinimalCommand
 import org.globalforestwatch.summarystats.carbon_sensitivity.CarbonSensitivityCommand.carbonSensitivityCommand
 import org.globalforestwatch.summarystats.carbonflux.CarbonFluxCommand.carbonFluxCommand
@@ -9,20 +8,29 @@ import org.globalforestwatch.summarystats.forest_change_diagnostic.ForestChangeD
 import org.globalforestwatch.summarystats.gfwpro_dashboard.GfwProDashboardCommand.gfwProDashboardCommand
 import org.globalforestwatch.summarystats.gladalerts.GladAlertsCommand.gladAlertsCommand
 import org.globalforestwatch.summarystats.treecoverloss.TreeCoverLossCommand.treeCoverLossCommand
+import com.monovore.decline._
 
-object SummaryMain
-  extends CommandApp(
-    name = "geotrellis-summary-stats",
-    header = "Compute summary statistics for GFW data",
-    main = {
-      annualupdateMinimalCommand orElse
-        carbonSensitivityCommand orElse
-        carbonFluxCommand orElse
-        fireAlertsCommand orElse
-        forestChangeDiagnosticCommand orElse
-        gfwProDashboardCommand orElse
-        gladAlertsCommand orElse
-        treeCoverLossCommand
+object SummaryMain {
+  val name = "geotrellis-summary-stats"
+  val header = "Compute summary statistics for GFW data"
+  val main = {
+    annualupdateMinimalCommand orElse
+      carbonSensitivityCommand orElse
+      carbonFluxCommand orElse
+      fireAlertsCommand orElse
+      forestChangeDiagnosticCommand orElse
+      gfwProDashboardCommand orElse
+      gladAlertsCommand orElse
+      treeCoverLossCommand
+  }
+  val command = Command(name, header, true)(main)
 
+  final def main(args: Array[String]): Unit = {
+    command.parse(args, sys.env) match {
+      case Left(help) =>
+        System.err.println(help)
+        System.exit(2)
+      case Right(_) => ()
     }
-  )
+  }
+}
