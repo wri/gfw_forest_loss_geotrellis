4f3da77ddc816deabdcabf75e500df0c12839fcb
Justin Terry <justin.terry@wri.org>
2022-12-28 11:51:21 -0800

    Don't remove dissolved geom

--- src/main/scala/org/globalforestwatch/util/GridRDD.scala
+++ src/main/scala/org/globalforestwatch/util/GridRDD.scala
@@ -9,7 +9,6 @@ import org.globalforestwatch.util.GeometryConstructor.createPolygon1x1
 
 object GridRDD {
   def apply(envelope: Envelope, spark: SparkSession, clip: Boolean = false): PolygonRDD = {
-
     val gridCells = getGridCells(envelope)
 
     val tcl_geom: Geometry = TreeCoverLossExtent.geometry //new GeometryFactory().toGeometry(new Envelope(-180, 180, -90, 90))
