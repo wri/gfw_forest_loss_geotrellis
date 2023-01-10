264d4199d5a2d6d2966e23a6795b1ad4ccb07bfa
Solomon Negusse <solomon.negusse@wri.org>
2023-01-10 08:18:37 -0800

    update Prodes tiles

--- src/main/scala/org/globalforestwatch/summarystats/forest_change_diagnostic/ForestChangeDiagnosticTile.scala
+++ src/main/scala/org/globalforestwatch/summarystats/forest_change_diagnostic/ForestChangeDiagnosticTile.scala
@@ -3,29 +3,27 @@ package org.globalforestwatch.summarystats.forest_change_diagnostic
 import geotrellis.raster.{CellGrid, CellType}
 import org.globalforestwatch.layers._
 
-/**
-  *
-  * Tile-like structure to hold tiles from datasets required for our summary.
-  * We can not use GeoTrellis MultibandTile because it requires all bands share a CellType.
+/** Tile-like structure to hold tiles from datasets required for our summary. We can not use GeoTrellis MultibandTile because it requires
+  * all bands share a CellType.
   */
 case class ForestChangeDiagnosticTile(
-                                       loss: TreeCoverLoss#ITile,
-                                       tcd2000: TreeCoverDensityPercent2000#ITile,
-                                       isPrimaryForest: PrimaryForest#OptionalITile,
-                                       isPeatlands: Peatlands#OptionalITile,
-                                       isIntactForestLandscapes2000: IntactForestLandscapes2000#OptionalITile,
-                                       wdpaProtectedAreas: ProtectedAreas#OptionalITile,
-                                       seAsiaLandCover: SEAsiaLandCover#OptionalITile,
-                                       idnLandCover: IndonesiaLandCover#OptionalITile,
-                                       isSoyPlantedArea: SoyPlantedAreas#OptionalITile,
-                                       idnForestArea: IndonesiaForestArea#OptionalITile,
-                                       isIDNForestMoratorium: IndonesiaForestMoratorium#OptionalITile,
-                                       prodesLossYear: ProdesLossYear#OptionalITile,
-                                       braBiomes: BrazilBiomes#OptionalITile,
-                                       isPlantation: PlantedForestsBool#OptionalITile,
-                                       gfwProCoverage: GFWProCoverage#OptionalITile
-
-                                     ) extends CellGrid[Int] {
+  loss: TreeCoverLoss#ITile,
+  tcd2000: TreeCoverDensityPercent2000#ITile,
+  isPrimaryForest: PrimaryForest#OptionalITile,
+  isPeatlands: Peatlands#OptionalITile,
+  isIntactForestLandscapes2000: IntactForestLandscapes2000#OptionalITile,
+  wdpaProtectedAreas: ProtectedAreas#OptionalITile,
+  seAsiaLandCover: SEAsiaLandCover#OptionalITile,
+  idnLandCover: IndonesiaLandCover#OptionalITile,
+  isSoyPlantedArea: SoyPlantedAreas#OptionalITile,
+  idnForestArea: IndonesiaForestArea#OptionalITile,
+  isIDNForestMoratorium: IndonesiaForestMoratorium#OptionalITile,
+  prodesAmazonLossYear: ProdesAmazonLossYear#OptionalITile,
+  prodesCerradoLossYear: ProdesCerradoLossYear#OptionalITile,
+  braBiomes: BrazilBiomes#OptionalITile,
+  isPlantation: PlantedForestsBool#OptionalITile,
+  gfwProCoverage: GFWProCoverage#OptionalITile
+) extends CellGrid[Int] {
 
   def cellType: CellType = loss.cellType
 
