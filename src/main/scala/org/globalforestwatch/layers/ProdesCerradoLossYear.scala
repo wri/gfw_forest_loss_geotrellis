dea754dca6fa5d727d59d258133fab1d231d0e36
Solomon Negusse <solomon.negusse@wri.org>
2023-01-31 10:12:34 +0300

    update prodes with kwargs and tests

--- src/main/scala/org/globalforestwatch/layers/ProdesCerradoLossYear.scala
+++ src/main/scala/org/globalforestwatch/layers/ProdesCerradoLossYear.scala
@@ -5,7 +5,7 @@ import org.globalforestwatch.grids.GridTile
 case class ProdesCerradoLossYear(gridTile: GridTile, kwargs: Map[String, Any]) extends IntegerLayer with OptionalILayer {
   val datasetName = "inpe_cerrado_prodes"
   val uri: String =
-    uriForGrid(gridTile)
+    uriForGrid(gridTile, kwargs)
 
   override def lookup(value: Int): Integer =
     if (value == 0) null else value
