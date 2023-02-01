39a29c749b4709f6fd2ca8b03caca7e190ed6490
Justin Terry <justin.terry@wri.org>
2023-02-01 10:22:07 -0800

    Fix compilation issues

--- src/main/scala/org/globalforestwatch/layers/ProdesAmazonLossYear.scala
+++ src/main/scala/org/globalforestwatch/layers/ProdesAmazonLossYear.scala
@@ -5,7 +5,7 @@ import org.globalforestwatch.grids.GridTile
 case class ProdesAmazonLossYear(gridTile: GridTile, kwargs: Map[String, Any]) extends IntegerLayer with OptionalILayer {
   val datasetName = "inpe_amazon_prodes"
   val uri: String =
-    uriForGrid(gridTile)
+    uriForGrid(gridTile, kwargs)
 
   override def lookup(value: Int): Integer =
     if (value == 0) null else value
