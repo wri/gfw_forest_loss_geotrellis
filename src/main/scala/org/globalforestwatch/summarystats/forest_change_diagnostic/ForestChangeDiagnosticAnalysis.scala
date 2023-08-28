4db5c2946745c0245d58e3a4224cd23848c639e0
Dan Scales <dan.scales@gmail.com>
2023-08-28 11:18:47 -0700

    Revert TCL 2022 changes for now
    Front-end not ready for the new data yet.

--- src/main/scala/org/globalforestwatch/summarystats/forest_change_diagnostic/ForestChangeDiagnosticAnalysis.scala
+++ src/main/scala/org/globalforestwatch/summarystats/forest_change_diagnostic/ForestChangeDiagnosticAnalysis.scala
@@ -91,10 +91,6 @@ object ForestChangeDiagnosticAnalysis extends SummaryAnalysis {
                   } else {
                     data.copy(
                       commodity_threat_fires = fire.getOrElse(ForestChangeDiagnosticDataLossYearly.empty),
-                      // Soy is planted late in year (Sept/Oct) and harvested in
-                      // March. So, the most recent data relates to soy planted late
-                      // in previous year. So, we should only intersect with tree
-                      // cover loss from previous year.
                       tree_cover_loss_soy_yearly = data.tree_cover_loss_soy_yearly.limitToMaxYear(2021)
                     )
                   }
@@ -201,8 +197,6 @@ object ForestChangeDiagnosticAnalysis extends SummaryAnalysis {
         usingIndex = true
       )
 
-    // This fire data is an input to the palm risk tool, so limit data to 2021 to sync
-    // with the palm risk tool.
     joinedRDD.rdd
       .map { case (poly, points) =>
         val fid = poly.getUserData.asInstanceOf[FeatureId]
