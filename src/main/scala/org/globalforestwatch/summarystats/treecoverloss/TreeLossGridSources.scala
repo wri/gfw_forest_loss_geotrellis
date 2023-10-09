3013a3efdf1559ccf479157574b0c750fe0cd395
dagibbs22 <dagibbs22@gmail.com>
2023-10-08 21:54:30 -0400

    Added print statements to layer definitions, per Dan's lazy loading commit: https://github.com/wri/gfw_forest_loss_geotrellis/commit/22da705f60ade00c78b96a7d757850e2a8876137#diff-c55ddb06e7648155cdd04a32f4566e09ff7d6516f337febc38a5587c419da89d. Added lazy loading properties to files per the same commit of Dan's.

--- src/main/scala/org/globalforestwatch/summarystats/treecoverloss/TreeLossGridSources.scala
+++ src/main/scala/org/globalforestwatch/summarystats/treecoverloss/TreeLossGridSources.scala
@@ -28,67 +28,73 @@ case class TreeLossGridSources(gridTile: GridTile, kwargs: Map[String, Any]) ext
   val grossCumulBelowgroundRemovalsCo2: GrossCumulBelowgroundRemovalsCo2 = GrossCumulBelowgroundRemovalsCo2(gridTile, kwargs = kwargs)
   val grossEmissionsCo2eNonCo2: GrossEmissionsNonCo2Co2e = GrossEmissionsNonCo2Co2e(gridTile, kwargs = kwargs)
   val grossEmissionsCo2eCo2Only: GrossEmissionsCo2OnlyCo2e = GrossEmissionsCo2OnlyCo2e(gridTile, kwargs = kwargs)
-  val netFluxCo2: NetFluxCo2e = NetFluxCo2e(gridTile, kwargs = kwargs)
+  val netFluxCo2e: NetFluxCo2e = NetFluxCo2e(gridTile, kwargs = kwargs)
   val fluxModelExtent: FluxModelExtent = FluxModelExtent(gridTile, kwargs = kwargs)
 
 
-  def readWindow(windowKey: SpatialKey, windowLayout: LayoutDefinition): Either[Throwable, Raster[TreeLossTile]] = {
+  def readWindow(windowKey: SpatialKey, windowLayout: LayoutDefinition):
+  Either[Throwable, Raster[TreeLossTile]] = {
 
-    for {
-      // Failure for any of these reads will result in function returning Left[Throwable]
-      // These are effectively required fields without which we can't make sense of the analysis
-      lossTile <- Either.catchNonFatal(treeCoverLoss.fetchWindow(windowKey, windowLayout)).right
-      tcd2000Tile <- Either
-        .catchNonFatal(treeCoverDensity2000.fetchWindow(windowKey, windowLayout))
-        .right
-      tcd2010Tile <- Either
-        .catchNonFatal(treeCoverDensity2010.fetchWindow(windowKey, windowLayout))
-        .right
-
-    } yield {
-      // Failure for these will be converted to optional result and propagated with TreeLossTile
-      val gainTile = treeCoverGain.fetchWindow(windowKey, windowLayout)
-      val primaryForestTile = primaryForest.fetchWindow(windowKey, windowLayout)
-      val plantedForestsBoolTile = plantedForestsBool.fetchWindow(windowKey, windowLayout)
-      val plantationsPre2000Tile = plantationsPre2000.fetchWindow(windowKey, windowLayout)
-
-      val biomassTile = biomassPerHectar.fetchWindow(windowKey, windowLayout)
-      val agc2000Tile = agc2000.fetchWindow(windowKey, windowLayout)
-      val bgc2000Tile = bgc2000.fetchWindow(windowKey, windowLayout)
-      val soilCarbon2000Tile = soilCarbon2000.fetchWindow(windowKey, windowLayout)
-
-      val grossCumulAbovegroundRemovalsCo2Tile = grossCumulAbovegroundRemovalsCo2.fetchWindow(windowKey, windowLayout)
-      val grossCumulBelowgroundRemovalsCo2Tile = grossCumulBelowgroundRemovalsCo2.fetchWindow(windowKey, windowLayout)
-      val grossEmissionsCo2eNonCo2Tile = grossEmissionsCo2eNonCo2.fetchWindow(windowKey, windowLayout)
-      val grossEmissionsCo2eCo2OnlyTile = grossEmissionsCo2eCo2Only.fetchWindow(windowKey, windowLayout)
-      val netFluxCo2Tile = netFluxCo2.fetchWindow(windowKey, windowLayout)
-      val fluxModelExtentTile = fluxModelExtent.fetchWindow(windowKey, windowLayout)
-      val mangroveBiomassExtentTile =
-        mangroveBiomassExtent.fetchWindow(windowKey, windowLayout)
-
-      val tile = TreeLossTile(
-        lossTile,
-        gainTile,
-        tcd2000Tile,
-        tcd2010Tile,
-        biomassTile,
-        agc2000Tile,
-        bgc2000Tile,
-        soilCarbon2000Tile,
-        primaryForestTile,
-        plantedForestsBoolTile,
-        plantationsPre2000Tile,
-        mangroveBiomassExtentTile,
-        grossCumulAbovegroundRemovalsCo2Tile,
-        grossCumulBelowgroundRemovalsCo2Tile,
-        netFluxCo2Tile,
-        grossEmissionsCo2eNonCo2Tile,
-        grossEmissionsCo2eCo2OnlyTile,
-        fluxModelExtentTile,
-      )
-
-      Raster(tile, windowKey.extent(windowLayout))
-    }
+    val tile = TreeLossTile(
+      windowKey, windowLayout, this
+    )
+    Right(Raster(tile, windowKey.extent(windowLayout)))
+//
+//    for {
+//      // Failure for any of these reads will result in function returning Left[Throwable]
+//      // These are effectively required fields without which we can't make sense of the analysis
+//      lossTile <- Either.catchNonFatal(treeCoverLoss.fetchWindow(windowKey, windowLayout)).right
+//      tcd2000Tile <- Either
+//        .catchNonFatal(treeCoverDensity2000.fetchWindow(windowKey, windowLayout))
+//        .right
+//      tcd2010Tile <- Either
+//        .catchNonFatal(treeCoverDensity2010.fetchWindow(windowKey, windowLayout))
+//        .right
+//
+//    } yield {
+//      // Failure for these will be converted to optional result and propagated with TreeLossTile
+//      val gainTile = treeCoverGain.fetchWindow(windowKey, windowLayout)
+//      val primaryForestTile = primaryForest.fetchWindow(windowKey, windowLayout)
+//      val plantedForestsBoolTile = plantedForestsBool.fetchWindow(windowKey, windowLayout)
+//      val plantationsPre2000Tile = plantationsPre2000.fetchWindow(windowKey, windowLayout)
+//
+//      val biomassTile = biomassPerHectar.fetchWindow(windowKey, windowLayout)
+//      val agc2000Tile = agc2000.fetchWindow(windowKey, windowLayout)
+//      val bgc2000Tile = bgc2000.fetchWindow(windowKey, windowLayout)
+//      val soilCarbon2000Tile = soilCarbon2000.fetchWindow(windowKey, windowLayout)
+//
+//      val grossCumulAbovegroundRemovalsCo2Tile = grossCumulAbovegroundRemovalsCo2.fetchWindow(windowKey, windowLayout)
+//      val grossCumulBelowgroundRemovalsCo2Tile = grossCumulBelowgroundRemovalsCo2.fetchWindow(windowKey, windowLayout)
+//      val grossEmissionsCo2eNonCo2Tile = grossEmissionsCo2eNonCo2.fetchWindow(windowKey, windowLayout)
+//      val grossEmissionsCo2eCo2OnlyTile = grossEmissionsCo2eCo2Only.fetchWindow(windowKey, windowLayout)
+//      val netFluxCo2Tile = netFluxCo2.fetchWindow(windowKey, windowLayout)
+//      val fluxModelExtentTile = fluxModelExtent.fetchWindow(windowKey, windowLayout)
+//      val mangroveBiomassExtentTile =
+//        mangroveBiomassExtent.fetchWindow(windowKey, windowLayout)
+//
+//      val tile = TreeLossTile(
+//        lossTile,
+//        gainTile,
+//        tcd2000Tile,
+//        tcd2010Tile,
+//        biomassTile,
+//        agc2000Tile,
+//        bgc2000Tile,
+//        soilCarbon2000Tile,
+//        primaryForestTile,
+//        plantedForestsBoolTile,
+//        plantationsPre2000Tile,
+//        mangroveBiomassExtentTile,
+//        grossCumulAbovegroundRemovalsCo2Tile,
+//        grossCumulBelowgroundRemovalsCo2Tile,
+//        netFluxCo2Tile,
+//        grossEmissionsCo2eNonCo2Tile,
+//        grossEmissionsCo2eCo2OnlyTile,
+//        fluxModelExtentTile,
+//      )
+//
+//      Raster(tile, windowKey.extent(windowLayout))
+//    }
   }
 }
 
--- src/main/scala/org/globalforestwatch/summarystats/treecoverloss/TreeLossTile.scala
+++ src/main/scala/org/globalforestwatch/summarystats/treecoverloss/TreeLossTile.scala
@@ -1,6 +1,7 @@
 package org.globalforestwatch.summarystats.treecoverloss
 
 import geotrellis.raster.{CellGrid, CellType}
+import geotrellis.layer.{LayoutDefinition, SpatialKey}
 import org.globalforestwatch.layers._
 
 /**
@@ -8,30 +9,54 @@ import org.globalforestwatch.layers._
   * Tile-like structure to hold tiles from datasets required for our summary.
   * We can not use GeoTrellis MultibandTile because it requires all bands share a CellType.
   */
-case class TreeLossTile(loss: TreeCoverLoss#ITile,
-                        gain: TreeCoverGain#OptionalITile,
-                        tcd2000: TreeCoverDensityPercent2000#ITile,
-                        tcd2010: TreeCoverDensityPercent2010#ITile,
-                        biomass: BiomassPerHectar#OptionalDTile,
-                        agc2000: Agc2000#OptionalFTile,
-                        bgc2000: Bgc2000#OptionalFTile,
-                        soilCarbon2000: SoilCarbon2000#OptionalFTile,
-                        primaryForest: PrimaryForest#OptionalITile,
-                        plantedForestsBool: PlantedForestsBool#OptionalITile,
-                        plantationsPre2000: PlantationsPre2000#OptionalITile,
-                        mangroveBiomassExtent: MangroveBiomassExtent#OptionalITile,
-                        grossCumulAbovegroundRemovalsCo2: GrossCumulAbovegroundRemovalsCo2#OptionalFTile,
-                        grossCumulBelowgroundRemovalsCo2: GrossCumulBelowgroundRemovalsCo2#OptionalFTile,
-                        netFluxCo2: NetFluxCo2e#OptionalFTile,
-                        grossEmissionsCo2eNonCo2: GrossEmissionsNonCo2Co2e#OptionalFTile,
-                        grossEmissionsCo2eCo2Only: GrossEmissionsCo2OnlyCo2e#OptionalFTile,
-                        fluxModelExtent: FluxModelExtent#OptionalITile)
+case class TreeLossTile(
+                         windowKey: SpatialKey,
+                         windowLayout: LayoutDefinition,
+                         sources: TreeLossGridSources,
 
+//                        loss: TreeCoverLoss#ITile,
+//                        gain: TreeCoverGain#OptionalITile,
+//                        tcd2000: TreeCoverDensityPercent2000#ITile,
+//                        tcd2010: TreeCoverDensityPercent2010#ITile,
+//                        biomass: BiomassPerHectar#OptionalDTile,
+//                        agc2000: Agc2000#OptionalFTile,
+//                        bgc2000: Bgc2000#OptionalFTile,
+//                        soilCarbon2000: SoilCarbon2000#OptionalFTile,
+//                        primaryForest: PrimaryForest#OptionalITile,
+//                        plantedForestsBool: PlantedForestsBool#OptionalITile,
+//                        plantationsPre2000: PlantationsPre2000#OptionalITile,
+//                        mangroveBiomassExtent: MangroveBiomassExtent#OptionalITile,
+//                        grossCumulAbovegroundRemovalsCo2: GrossCumulAbovegroundRemovalsCo2#OptionalFTile,
+//                        grossCumulBelowgroundRemovalsCo2: GrossCumulBelowgroundRemovalsCo2#OptionalFTile,
+//                        netFluxCo2: NetFluxCo2e#OptionalFTile,
+//                        grossEmissionsCo2eNonCo2: GrossEmissionsNonCo2Co2e#OptionalFTile,
+//                        grossEmissionsCo2eCo2Only: GrossEmissionsCo2OnlyCo2e#OptionalFTile,
+//                        fluxModelExtent: FluxModelExtent#OptionalITile)
+                       )
   extends CellGrid[Int] {
 
-  def cellType: CellType = loss.cellType
+    lazy val loss = sources.treeCoverLoss.fetchWindow(windowKey, windowLayout)
+    lazy val gain = sources.treeCoverGain.fetchWindow(windowKey, windowLayout)
+    lazy val tcd2000 = sources.treeCoverDensity2000.fetchWindow(windowKey, windowLayout)
+    lazy val tcd2010 = sources.treeCoverDensity2010.fetchWindow(windowKey, windowLayout)
+    lazy val biomass = sources.biomassPerHectar.fetchWindow(windowKey, windowLayout)
+    lazy val agc2000 = sources.agc2000.fetchWindow(windowKey, windowLayout)
+    lazy val bgc2000 = sources.bgc2000.fetchWindow(windowKey, windowLayout)
+    lazy val soilCarbon2000 = sources.soilCarbon2000.fetchWindow(windowKey, windowLayout)
+    lazy val primaryForest = sources.primaryForest.fetchWindow(windowKey, windowLayout)
+    lazy val plantedForestsBool = sources.plantedForestsBool.fetchWindow(windowKey, windowLayout)
+    lazy val plantationsPre2000 = sources.plantationsPre2000.fetchWindow(windowKey, windowLayout)
+    lazy val mangroveBiomassExtent = sources.mangroveBiomassExtent.fetchWindow(windowKey, windowLayout)
+    lazy val grossCumulAbovegroundRemovalsCo2 = sources.grossCumulAbovegroundRemovalsCo2.fetchWindow(windowKey, windowLayout)
+    lazy val grossCumulBelowgroundRemovalsCo2 = sources.grossCumulBelowgroundRemovalsCo2.fetchWindow(windowKey, windowLayout)
+    lazy val netFluxCo2 = sources.netFluxCo2e.fetchWindow(windowKey, windowLayout)
+    lazy val grossEmissionsCo2eNonCo2 = sources.grossEmissionsCo2eNonCo2.fetchWindow(windowKey, windowLayout)
+    lazy val grossEmissionsCo2eCo2Only = sources.grossEmissionsCo2eCo2Only.fetchWindow(windowKey, windowLayout)
+    lazy val fluxModelExtent = sources.fluxModelExtent.fetchWindow(windowKey, windowLayout)
 
-  def cols: Int = loss.cols
+    def cellType: CellType = loss.cellType
 
-  def rows: Int = loss.rows
+    def cols: Int = loss.cols
+
+    def rows: Int = loss.rows
 }
