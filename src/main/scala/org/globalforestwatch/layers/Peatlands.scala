2e616348d9ca8672a869543695ca90f413ab1076
dagibbs22 <dagibbs22@gmail.com>
2023-11-02 09:23:24 -0400

    Changed flux model layer sources back to gfw-data-lake from gfw-files.

--- src/main/scala/org/globalforestwatch/layers/Peatlands.scala
+++ src/main/scala/org/globalforestwatch/layers/Peatlands.scala
@@ -10,11 +10,4 @@ case class Peatlands(gridTile: GridTile, kwargs: Map[String, Any])
 
     val uri: String =
       uriForGrid(gridTile, kwargs)
-
-
-//  // For carbonflux package run only
-//  val datasetName = "Na"
-//
-//  val uri: String =
-//    s"s3://gfw-files/flux_1_2_3/peatlands_flux_extent/standard/${gridTile.tileId}.tif"
 }
