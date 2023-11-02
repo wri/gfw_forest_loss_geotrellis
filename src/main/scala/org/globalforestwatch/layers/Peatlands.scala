369797f89f62f8c971f213cf0df7dff74ca77fdc
dagibbs22 <dagibbs22@gmail.com>
2023-11-02 09:23:08 -0400

    Changed flux model layer sources back to gfw-data-lake from gfw-files.

--- src/main/scala/org/globalforestwatch/layers/Peatlands.scala
+++ src/main/scala/org/globalforestwatch/layers/Peatlands.scala
@@ -6,15 +6,15 @@ case class Peatlands(gridTile: GridTile, kwargs: Map[String, Any])
   extends BooleanLayer
     with OptionalILayer {
 
-//    val datasetName = "gfw_peatlands"
+    val datasetName = "gfw_peatlands"
+
+    val uri: String =
+      uriForGrid(gridTile, kwargs)
+
+
+//  // For carbonflux package run only
+//  val datasetName = "Na"
 //
-//    val uri: String =
-//      uriForGrid(gridTile, kwargs)
-
-
-  // For carbonflux package run only
-  val datasetName = "Na"
-
-  val uri: String =
-    s"s3://gfw-files/flux_1_2_3/peatlands_flux_extent/standard/${gridTile.tileId}.tif"
+//  val uri: String =
+//    s"s3://gfw-files/flux_1_2_3/peatlands_flux_extent/standard/${gridTile.tileId}.tif"
 }
