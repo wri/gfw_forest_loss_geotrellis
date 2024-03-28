16e7a98c594c76aa2d88dcc433ad6512b3b2d0c3
Dan Scales <dan.scales@gmail.com>
2024-03-28 15:46:03 -0700

    Change geotrellis to 3.7.0 (from 3.7.0-SNAPSHOT)
    geotrellis release 3.7.0-SNAPSHOT just got removed from
    https://repo1.maven.org/maven2/org/locationtech/geotrellis/ today or
    late yesterday. So, we are forced to move to 3.7.0. When we move to
    3.7.0, we also have to upgrade to Spark 3.3.2 and frameless 0.13.0, else
    dependencies don't work and things don't compile.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,14 +20,14 @@ object Version {
   val breeze = "0.13.2"
   val decline = "1.3.0"
   val scala = "2.12.12"
-  val geotrellis = "3.7.0-SNAPSHOT"
+  val geotrellis = "3.7.0"
   val hadoop = "3.2.1"
   val jackson = "2.10.0"
-  val spark = "3.1.1"
+  val spark = "3.3.2"
   val sparkCompatible = "3.0"
   val geotools = "24.1"
   val sedona = "1.2.0-incubating"
-  val frameless = "0.9.0"
+  val frameless = "0.13.0"
   val jts = "1.19.0"
 }
 
