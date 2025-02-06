479bec683be2db18747e65bea5157827086bbe95
Dan Scales <dan.scales@gmail.com>
2025-02-06 13:38:13 -0800

    Filter out any rows with location_id == -1 for GHG.
    Dissolved list geometries don't make sense for GHG analysis, since
    different locations on the same list can have different commodities or
    yields.

--- src/main/scala/org/globalforestwatch/summarystats/ghg/GHGAnalysis.scala
+++ src/main/scala/org/globalforestwatch/summarystats/ghg/GHGAnalysis.scala
@@ -10,14 +10,15 @@ import org.apache.spark.sql.SparkSession
 import org.globalforestwatch.summarystats.{Location, NoIntersectionError, SummaryAnalysis, ValidatedLocation}
 import org.apache.spark.storage.StorageLevel
 import org.globalforestwatch.ValidatedWorkflow
+import org.globalforestwatch.features.GfwProFeatureExtId
 
 object GHGAnalysis extends SummaryAnalysis {
 
   val name = "ghg"
 
   /** Greenhouse gas analysis of input features in a TSV file. The TSV file contains
-    * the individual list items (location IDs >= 0) and optional merged ("dissolved")
-    * list geometries (location id -1).
+    * the individual list items (location IDs >= 0). Any option merged ("dissolved")
+    * list geometries (location id -1) are ignored.
     *
     * This function assumes that all features have already been split by 1x1 degree
     * grid, so each location and merged list may have a single or multiple rows.
@@ -28,9 +29,17 @@ object GHGAnalysis extends SummaryAnalysis {
   )(implicit spark: SparkSession): RDD[ValidatedLocation[GHGData]] = {
     features.persist(StorageLevel.MEMORY_AND_DISK)
 
+    // Drop any rows with location id of -1 (dissolved area of whole list), since
+    // that doesn't make sense for GHG analysis, since different locations on a list
+    // can have different commodities or yields that are used in the analysis.
+    val dropDissolvedRowsRDD = features.filter{
+      case Valid(Location(GfwProFeatureExtId(_, locationId, _, _), geom)) => {
+        locationId != -1
+      }
+    }
     try {
       val partialResult: RDD[ValidatedLocation[GHGData]] = {
-        ValidatedWorkflow(features)
+        ValidatedWorkflow(dropDissolvedRowsRDD)
           .flatMap { locationGeometries =>
             val locationSummaries: RDD[ValidatedLocation[GHGSummary]] = {
               val tmp = locationGeometries.map { case Location(id, geom) => Feature(geom, id) }
