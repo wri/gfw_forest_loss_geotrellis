f814f28ce5fe70da0d11019d5b4ee7923d085135
Dan Scales <dan.scales@gmail.com>
2025-12-07 08:17:48 -0800

    Use DateConfLayer for DistAlerts layer
    I am using the resampled10m raster of dist alerts, which already has
    adjusted the dates to be based on 2014/12/31, so don't use
    DistDateConfLayer, since that would incorrectly adjust the dates again.

--- src/main/scala/org/globalforestwatch/layers/DistAlerts.scala
+++ src/main/scala/org/globalforestwatch/layers/DistAlerts.scala
@@ -2,7 +2,7 @@ package org.globalforestwatch.layers
 
 import org.globalforestwatch.grids.GridTile
 
-case class DistAlerts(gridTile: GridTile, kwargs: Map[String, Any]) extends DistDateConfLayer with OptionalILayer {
+case class DistAlerts(gridTile: GridTile, kwargs: Map[String, Any]) extends DateConfLayer with OptionalILayer {
   val datasetName = "umd_glad_dist_alerts"
 
   val uri: String =
