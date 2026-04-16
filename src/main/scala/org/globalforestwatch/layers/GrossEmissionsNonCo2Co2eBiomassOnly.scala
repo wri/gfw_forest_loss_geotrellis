7fd0853e9b65ee3066e2e360328bf25aa79cc534
Melissa Rose <137821682+melrose-wri@users.noreply.github.com>
2026-04-16 12:35:05 -0700

    Carbonflux Update:
    - Deleted commented out section in GrossEmissionsNonCo2Co2eBiomassOnly.scala

--- src/main/scala/org/globalforestwatch/layers/GrossEmissionsNonCo2Co2eBiomassOnly.scala
+++ src/main/scala/org/globalforestwatch/layers/GrossEmissionsNonCo2Co2eBiomassOnly.scala
@@ -11,10 +11,4 @@ case class GrossEmissionsNonCo2Co2eBiomassOnly(gridTile: GridTile, kwargs: Map[S
   val uri: String =
     uriForGrid(gridTile, kwargs)
 
-  //  // For carbon_sensitivity run only (but not currently functional)
-  //  val datasetName = "Na"
-  //
-  //  val model_suffix: String = if (model == "standard") "standard" else s"$model"
-  //  val uri: String =
-  //      s"s3://gfw-data-lake/gfw_forest_flux_full_extent_gross_emissions_non_co2_biomass_only/v20231114/raster/epsg-4326/{grid_size}/{row_count}/Mg_CO2e_ha-1/geotiff/{tile_id}.tif"
 }
